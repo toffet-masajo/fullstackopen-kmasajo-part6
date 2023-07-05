5543582903672b13106af44d9e0fbb6d81a8f0a3
Kristoffer Masajo <toffet-masajo@users.noreply.github.com>
2023-07-06 02:17:56 +0800

    Exercise 6.20

--- query-anecdotes/src/App.js
+++ query-anecdotes/src/App.js
@@ -1,5 +1,8 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+import AnecdoteForm from './components/AnecdoteForm';
+import Notification from './components/Notification';
 
 const App = () => {
 
@@ -7,13 +10,22 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const getAnecdotes = () => {
+    return axios
+      .get('http://localhost:3001/anecdotes')
+      .then(res => res.data)
+  }
+
+  const result = useQuery(
+    'anecdotes',
+    getAnecdotes,
+    { retry: false }
+  );
+
+  if(result.isLoading) return <div>loading data...</div>;
+  if(result.isError) return <div>anecdote service not available due to problems in server</div>
+
+  const anecdotes = result.data;
 
   return (
     <div>
